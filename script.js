15175aeb83682b1d6ebaeea68dee852676b2fd21
lnginyard <lnginyard@gmail.com>
2022-12-05 18:54:55 -0500

    refactor code

--- script.js
+++ script.js
@@ -5,34 +5,39 @@ let flipBtn = document.querySelector("#flip-button");
 let resetBtn = document.querySelector("#reset-button");
 
 flipBtn.addEventListener("click", () => {
-    let i = Math.floor(Math.random() * 2);
+    let random = Math.random();
     coin.style.animation = "none";
-    if(i){
-        setTimeout(function(){
+    if (random < 0.5) {
+        setTimeout(function() {
             coin.style.animation = "spin-heads 3s forwards";
         }, 100);
         heads++;
-    }
-    else{
-        setTimeout(function(){
+        coin.src = "heads.png";
+    } else {
+        setTimeout(function() {
             coin.style.animation = "spin-tails 3s forwards";
         }, 100);
         tails++;
+        coin.src = "tails.png";
     }
     setTimeout(updateStats, 3000);
     disableButton();
 });
-function updateStats(){
+
+
+
+function updateStats() {
     document.querySelector("#heads-count").textContent = `Heads: ${heads}`;
     document.querySelector("#tails-count").textContent = `Tails: ${tails}`;
 }
-function disableButton(){
+
+function disableButton() {
     flipBtn.disabled = true;
-    setTimeout(function(){
+    setTimeout(function() {
         flipBtn.disabled = false;
-    },3000);
+    }, 3000);
 }
-resetBtn.addEventListener("click",() => {
+resetBtn.addEventListener("click", () => {
     coin.style.animation = "none";
     heads = 0;
     tails = 0;
